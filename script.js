f9ff75cc4508cc5631337deafbf4e7b212c063df
Ryan Watts <ryanwatts21@gmail.com>
2017-11-19 01:15:33 +0000

    Update script.js

--- script.js
+++ script.js
@@ -123,22 +123,49 @@ $(function () {
 
 // Interactive
 
+var bands = [
+    [0,0,0,0],
+    [1,4,67,23],
+    [2,2,54,17],
+    [3,16,78,9],
+    [4,1,45,28],
+    [5,4,66,21],
+    [6,3,71,19],
+    [7,9,59,56],
+    [8,16,89,11],
+  ];
+
 function submitFunction() {
-    var x = document.getElementById('databit');
-    if (x.style.display !== 'none') {
-        x.style.display = 'none';
-    } else {
-      	x.style.display = 'block';
+
+    var band = document.getElementById('myDropdown').value;
+    var bandname = document.getElementById('myDropdown').selectedOptions[0].text;
+    var one = 0;
+    var two = 0;
+    var three = 0;
+
+    if (band == bands[band][0]) {
+        one = bands[band][1];
+        two = bands[band][2];
+        three = bands[band][3];
     }
-}
 
-//Changes the inner html for the databit
-var select = document.getElementById('myDropdown');
-var input = document.getElementById('name');
-select.onchange = function() {
-    document.getElementById('databitone').innerHTML = "<div>Guns N Roses vs " + select.value + "</div>"
-    document.getElementById('databittwo').innerHTML = "<div>Countries visited in 2016</div>"
-    document.getElementById('databitthree').innerHTML = "<div>Length of set by song length</div>"
-    document.getElementById('databitfour').innerHTML = "<div>Percentage of set from first album</div>"
+    var visitwidthleft = 100*18/(18 + one)
+    var lengthwidthleft = 100*69/(69 + two)
+    var albumwidthleft = 100*52/(52 + three)
+    var visitwidthright = 100*one/(18 + one)
+    var lengthwidthright = 100*two/(69 + two)
+    var albumwidthright = 100*three/(52 + three)
 
-}
+    document.getElementById('databitone').innerHTML = "<div><span class='spanone'>Guns N Roses</span> vs <span class='spantwo'>" + bandname + "</span></div>"
+    document.getElementById('databittwo').innerHTML = "<div>Countries visited in 2016</div><div class='leftbar' id='visitleft'>18</div><div class='rightbar' id='visitright'>" + one + "</div>"
+    document.getElementById('databitthree').innerHTML = "<div>Length of set by song length</div><div class='leftbar' id='lengthleft'>69</div><div class='rightbar' id='lengthright'>" + two + "</div>"
+    document.getElementById('databitfour').innerHTML = "<div>Percentage of set from first album</div><div class='leftbar' id='albumleft'>52</div><div class='rightbar' id='albumright'>" + three + "</div>"
+
+    document.getElementById('visitleft').style.width = visitwidthleft + "%";
+    document.getElementById('visitright').style.width = visitwidthright + "%";
+    document.getElementById('lengthleft').style.width = lengthwidthleft + "%";
+    document.getElementById('lengthright').style.width = lengthwidthright + "%";
+    document.getElementById('albumleft').style.width = albumwidthleft + "%";
+    document.getElementById('albumright').style.width = albumwidthright + "%";
+
+      }
